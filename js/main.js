3df3e2405b2b2130abcad26ad9764e0c846c5e12
kzmt4 <kuzup4ik@gmail.com>
2020-11-27 22:12:22 +0300

    create: добавил слайдер

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-var mySwiper = new Swiper('.swiper-container', {
+var hotelSlider = new Swiper('.hotel-slider', {
     // Optional parameters
    
     loop: true,
@@ -7,8 +7,8 @@ var mySwiper = new Swiper('.swiper-container', {
   
     // Navigation arrows
     navigation: {
-      nextEl: '.slider-button--next',
-      prevEl: '.slider-button--prev',
+      nextEl: '.hotel-slider__button--next',
+      prevEl: '.hotel-slider__button--prev',
     },
     
     keyboard: {
@@ -17,7 +17,26 @@ var mySwiper = new Swiper('.swiper-container', {
       }, 
   
     // And if we need scrollbar
-    scrollbar: {
-      el: '.swiper-scrollbar',
+    
+  })
+
+  var reviewsSlider = new Swiper('.reviews-slider', {
+    // Optional parameters
+   
+    loop: true,
+  
+    
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.reviews-slider__button--next',
+      prevEl: '.reviews-slider__button--prev',
     },
+    
+    keyboard: {
+        enabled: true,
+        onlyInViewport: false,
+      }, 
+  
+    
   })
